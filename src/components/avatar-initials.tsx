0013105865ab359f19deaa4204b2dd7b56304827
Jacob Lauemøller <jacob.lauemoeller@iteray.com>
2026-09-09 10:38:06 +0200

    Add common AvatarInitials component

---
+++ src/components/avatar-initials.tsx
@@ -0,0 +1,16 @@
+import { AvatarFallback } from "@/components/ui/avatar";
+import { User } from "@/lib/models/user";
+
+type AvatarInitialsProps = {
+  user: User;
+};
+
+export function AvatarInitials({ user }: AvatarInitialsProps) {
+  const initials = initial(user.firstName).toUpperCase() + initial(user.lastName).toUpperCase();
+
+  return <AvatarFallback className="bg-primary text-secondary font-bold">{initials}</AvatarFallback>;
+}
+
+function initial(name: string | undefined): string {
+  return name?.at(0) ?? "";
+}
